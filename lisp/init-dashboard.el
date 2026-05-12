283896efc5d35a1a6068842a726f16bbda75f231
DanisAlfonso <danis.ramirez.hn@gmail.com>
2026-05-12 02:22:42 +0200

    Remove recent files from dashboard items

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -35,7 +35,7 @@
           dashboard-center-content t
           dashboard-vertically-center-content t
           dashboard-show-shortcuts nil
-          dashboard-items '((recents  . 10))
+          dashboard-items '(())
 
           dashboard-startupify-list '(dashboard-insert-newline
                                       dashboard-insert-navigator
